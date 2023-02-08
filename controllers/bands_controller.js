419732c94f54fb6bd0d6a5417025829d3e4648f7
Hdesai97 <hdesai97@gmail.com>
2023-02-08 17:55:32 -0500

    adding changes to controllers

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -2,11 +2,17 @@
 const bands = require('express').Router()
 const db = require('../models')
 const {Band, MeetGreet, Event, SetTime} = db
+const {Op} = require('sequelize')
 
 //FIND ALL BANDS
 bands.get('/', async (req,res) =>{
     try {
-        const foundBands = await Band.findAll()
+        const foundBands = await Band.findAll()({
+            order: [[ 'avaliable_start_time', 'ASC' ]],
+            where: {
+                name: {[Op.like]: `%${req.query.name ? req.query.name : ''}%`}
+            }
+        })
         res.status(200).json(foundBands)
     } catch (error) {
         res.status(500).json(error)
@@ -46,6 +52,19 @@ bands.get('/:id', async (req, res) => {
     }
 
 })
+// CREATE A BAND
+bands.post('/', async (req, res) => {
+    try {
+        const newBand= await Band.create(req.body)
+        res.status(200).json({
+            message:'Successfully inserted a new band',
+            data: newBand
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+
+})
 // Update A Band
 bands.put('/:id', async (req, res) =>{
     try {
@@ -62,6 +81,22 @@ bands.put('/:id', async (req, res) =>{
     }
 }
 )
+// Delete A Band
+bands.delete('/:id', async (req, res) =>{
+    try{
+        const deletedBands= await Band.destroy({
+            where: {
+                band_id:req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully deleted ${deletedBands} band(s)`
+        })
+
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
 
 // EXPORT
 module.exports = bands
